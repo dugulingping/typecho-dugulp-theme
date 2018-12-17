8ce47ef1fb1120635f6db425bd916de9ee5dff2b
dugulp <1184662350@qq.com>
2018-12-17 14:53:13 +0800

    排版了下html 修复了footer不透明的bug

--- footer.php
+++ footer.php
@@ -1,34 +1,26 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
-
- <footer class="site-footer">
-
-	<div class="inner">
-
-		<section class="footer-social">
-      
-      <a href="<?php $this->options->wlink(); ?>" target="_blank" title="Weibo"><i class="fa fa-2x fa-fw fa-weibo"></i> <span class="hidden">Weibo</span></a>&nbsp;
-       
-      <a href="<?php $this->options->glink(); ?>" target="_blank" title="GitHub"><i class="fa fa-2x fa-fw fa-github"></i> <span class="hidden">GitHub</span></a>&nbsp;
-           
-      <a href="/feed/" target="_blank" title="RSS"><i class="fa fa-2x fa-fw fa-rss"></i> <span class="hidden">RSS</span></a>
-      
-  </section>
-
-		<section class="copyright">自豪的采用typecho | themes by <a rel="license" href="http://blog.icyuyan.net">独孤伶俜</a>
-			<br>
-			<br />本站原创作品采用<br /><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议</a><br />进行许可
-		</section>
-	</div>
-</footer>
-
-<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdn.bootcss.com/highlight.js/9.13.1/highlight.min.js"></script>
-<?php if ($this->options->Instantclick == 'able'): ?>
-<script src="https://cdn.bootcss.com/instantclick/3.0.1/instantclick.min.js" data-no-instant></script>
-<script data-no-instant>InstantClick.init();</script>
-<?php endif; ?>
-<script src="<?php echo $this->options->cdn_link.'/default.min.js';?>"></script>
-<script src="<?php echo $this->options->cdn_link.'/prism.js';?>"></script>
+	<footer class="site-footer">
+		<div class="inner">
+			<section class="footer-social">
+				<a href="<?php $this->options->wlink(); ?>" target="_blank" title="Weibo"><i class="fa fa-2x fa-fw fa-weibo"></i> <span class="hidden">Weibo</span></a>&nbsp;
+				<a href="<?php $this->options->glink(); ?>" target="_blank" title="GitHub"><i class="fa fa-2x fa-fw fa-github"></i> <span class="hidden">GitHub</span></a>&nbsp;
+				<a href="/feed/" target="_blank" title="RSS"><i class="fa fa-2x fa-fw fa-rss"></i> <span class="hidden">RSS</span></a>
+			</section>
+			<section class="copyright">
+				Powered By Typecho|themes by <a rel="license" href="http://blog.icyuyan.net">独孤伶俜</a>
+				<br /><br />
+				本站原创作品采用<br /><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议</a><br />进行许可
+			</section>
+		</div>
+	</footer>
+	<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdn.bootcss.com/highlight.js/9.13.1/highlight.min.js"></script>
+	<?php if ($this->options->Instantclick == 'able'): ?>
+	<script src="https://cdn.bootcss.com/instantclick/3.0.1/instantclick.min.js" data-no-instant></script>
+	<script data-no-instant>InstantClick.init();</script><?php endif; ?>
+	<script src="<?php echo $this->options->cdn_link.'/default.min.js';?>"></script>
+	<script src="<?php echo $this->options->cdn_link.'/prism.js';?>"></script>
+	<?php $this->footer(); ?>
 </body>
 </html>
 <?php if ($this->options->Compress == 'able'): ?>
--- index.php
+++ index.php
@@ -8,55 +8,31 @@
  *
  */
 
-//关闭错误提示
-error_reporting(E_ALL^E_NOTICE^E_WARNING);
+	//关闭错误提示
+	error_reporting(E_ALL^E_NOTICE^E_WARNING);
 
-//正模板
-if (!defined('__TYPECHO_ROOT_DIR__')) exit;
- $this->need('header.php');
- ?>
+	//正模板
+	if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+		$this->need('header.php');
+?>
+	<main class="content" role="main">
+	<?php while($this->next()): ?>
+		<article class="preview">
+			<header>
+				<h1 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h1>
+				<div class="post-meta"><?php $this->author->screenName(); ?> on <?php $this->category(','); ?> | <time datetime="<?php $this->date('c'); ?>"><?php $this->date('F j, Y'); ?></time></div>
+			</header>
+			<section class="post-excerpt">
+				<p><?php $this->excerpt(130, '...'); ?></p>
+				<p class="readmore"><a href="<?php $this->permalink() ?>">Read more <i class="fa fa-chevron-circle-right" style="padding-left: 5px;"></i></a></p>
+			</section>
+		</article>
+	<?php endwhile; ?>
+		<nav class="pagination" role="pagination">
+			<?php $this->pageLink('<xt class="newer-posts"><i class="fa fa-chevron-circle-left"></i> Newer</xt>'); ?>
+			<span class="page-number">Page <?php if($this->_currentPage>1) echo $this->_currentPage;  else echo 1;?> of <?php echo   ceil($this->getTotal() / $this->parameter->pageSize); ?></span>
+			<?php $this->pageLink('<xt class="older-posts">Older <i class="fa fa-chevron-circle-right"></i></xt>','next'); ?>
+		</nav>
+	</main>
 
-<main class="content" role="main">
-
-        <?php while($this->next()): ?>
-
-           <article class="preview">
-
-    <header>
-
-        <h1 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h1>
-
-        <div class="post-meta"><?php $this->author->screenName(); ?> on <?php $this->category(','); ?> | <time datetime="<?php $this->date('c'); ?>"><?php $this->date('F j, Y'); ?></time></div>
-
-    </header>
-
-    <section class="post-excerpt">
-        
-        <p><?php $this->excerpt(130, '...'); ?></p>
-        
-        <p class="readmore"><a href="<?php $this->permalink() ?>">Read more <i class="fa fa-chevron-circle-right" style="padding-left: 5px;"></i></a></p>
-
-    </section>
-
-</article>
-
-<?php endwhile; ?>
-
-<nav class="pagination" role="pagination">
-
-
-<?php $this->pageLink('<xt class="newer-posts"><i class="fa fa-chevron-circle-left"></i> Newer</xt>'); ?>
-
-    <span class="page-number">Page <?php if($this->_currentPage>1) echo $this->_currentPage;  else echo 1;?> of <?php echo   ceil($this->getTotal() / $this->parameter->pageSize); ?></span>
-
-<?php $this->pageLink('<xt class="older-posts">Older <i class="fa fa-chevron-circle-right"></i></xt>','next'); ?>
-
-
-
-    
-    
-</nav>
-
-
-    </main>
 <?php $this->need('footer.php'); ?>
